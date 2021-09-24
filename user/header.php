818153a4e9881aea925b803ec8d6094a83f46fed
Praveena-T <IT19243672@my.sliit.lk>
2021-09-24 21:41:37 +0530

    First Commit

---
+++ user/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Shop</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- custom css -->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- owl carousel link -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+</head>
+
+<body>
+    <div class="container-responsive">
+        <!-- nav start -->
+        <nav class="navbar navbar-expand-lg px-5 py-4">
+            <div class="container-fluid">
+                <a class="navbar-brand header-name text-dark" href="#">Online Books</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav m-lg-auto me-auto mb-2 mb-lg-0">
+                        <li class="nav-item px-3">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="nav-link" href="./books.html">Books</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="nav-link" href="./about_us.html">About Us</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="nav-link" href="./contact_us.html">Contact Us</a>
+                        </li>
+                        <li class="nav-item px-3">
+                            <a class="btn btn-warning nav-link" href="./login.html">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
---
+++ user/home.php
@@ -0,0 +1,21 @@
+<?php
+include './header.php';
+?>
+<div class="jumbotron jumbotron-fluid">
+    <div class="row ">
+        <div class="jumbotron-head">
+            <div class="card jumbotron-head" style="background: #252325; border: none;">
+                <h1 class="display-4 text-center">Order Books Today.</h1>
+                <h2 class="lead_ text-center">Purchase with <span class="jumbotron-text"> Online Book.</span></h2>
+                <center>
+                    <a class="btn btn-jumbotron btn-lg">Explore More Details</a>
+                </center>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php
+include './footer.php';
+?>
